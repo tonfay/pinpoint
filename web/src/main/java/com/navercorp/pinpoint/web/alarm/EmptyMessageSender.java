72209e8a8c1cc5e03d388eaa1ab3e9704d31b866
tengfei <tengfei@shouqiev.com>
2019-03-15 11:35:57 +0800

    修改短信通知接口

--- web/src/main/java/com/navercorp/pinpoint/web/alarm/EmptyMessageSender.java
+++ web/src/main/java/com/navercorp/pinpoint/web/alarm/EmptyMessageSender.java
@@ -16,15 +16,35 @@
 
 package com.navercorp.pinpoint.web.alarm;
 
-import com.navercorp.pinpoint.web.alarm.checker.AlarmChecker;
+import org.codehaus.jettison.json.JSONObject;
 
+import com.navercorp.pinpoint.web.alarm.checker.AlarmChecker;
+import org.codehaus.jettison.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  * @author minwoo.jung
  */
 public class EmptyMessageSender implements AlarmMessageSender {
-
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Override
     public void sendSms(AlarmChecker checker, int sequenceCount) {
+        //钉钉代码块begin
+        JSONObject object = new JSONObject();
+        try {
+            object.put("msgtype", "text");
+            JSONObject text = new JSONObject();
+            String msg = checker.getEmailMessage();
+            msg = msg.replaceAll("<br>", "\n");
+            text.put("content", msg);//这里用email的邮件内容即可
+            object.put("text", text);
+        } catch (JSONException e) {
+            logger.error("组装参数异常",e);
+        }
+        logger.info("发送钉钉begin");
+        HttpUtil.doPost("https://oapi.dingtalk.com/robot/send?access_token=b39e9d52b898af22bdecdf5adc01724961513281cca86b28bd7ad0579bf7649f", object);
+        logger.info("发送钉钉end");
+        //钉钉代码块end
     }
 
     @Override
